86c2b51c71eb47e36734cfa1b8beeb996df9335d
mansisahay3499 <97956554+mansisahay3499@users.noreply.github.com>
2022-02-09 17:48:51 +0530

    Add files via upload

---
+++ ELEARNING/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html Lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com">
+
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>ischool</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">ischool</a>
+    <span class="navbar-text">Learn and Implement</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+</button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav custom-nav pl-5">
+      <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Courses</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment status</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My profile</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+      <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact</a></li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid remove-vid-marg">
+<div class="vid-parent">
+        <video playinline autoplay muted loop>
+          <source src="video/mansisahay.mp4">
+        </video>
+        <div class="vid-overlay"></div>
+        </div>
+        <div class="vid-content">
+          <h1 class="my-content">Learn and Implement</small>
+          <small class="my-content">Learn and Implement</small> <br>
+          <a href="#" class="btn btn-danger">Get Started</a>
+
+        </div>
+        </div>
+        <div class="container-fluid bg-danger txt-banner">
+          <div class="row bottom-banner">
+            <div class="col-sm">
+              <h5><i class="fas fa-book-open mr-3"></i>100+ online courses</h5>
+</div>
+<div class="col-sm">
+  <h5><i class="fas fa-users mr-3"></i>Expert Instructers</h5>
+</div>
+<div class="col-sm">
+  <h5><i class="fas fa-keyboard mr-3"></i>Lifetime Access</h5>
+</div>
+<div class="col-sm">
+  <h5><i class="fas fa-rupee-sign mr-3"></i>Money back guarantee</h5>
+</div>
+
+
+<script src="js/jquery.min.js"></script>
+<sript src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+</body>
+</html>
